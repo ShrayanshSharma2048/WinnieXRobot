75278444d93d823d73ea43e710bc87c029613aba
ShrayanshSharma2048 <82173539+ShrayanshSharma2048@users.noreply.github.com>
2021-04-14 01:09:30 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -72,7 +72,6 @@ def get_readable_time(seconds: int) -> str:
 
     return ping_time
 
-START_IMG=(https://telegra.ph/file/0813c2a8fc91296d963bc.jpg)
 
 PM_START_TEXT = """
 Hi {}, my name is *Winnie*! 
